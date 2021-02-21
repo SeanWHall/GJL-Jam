ca72cea183f835b1902e1b2e29c58d6ba2656410
Sean Hall <sean.hall412@gmail.com>
2021-02-21 20:44:04 +0000

    Setup a GameManager Prefab, so we can have persistent access to certain prefabs Added a BaseBehaviour, this will be the base script for all components. Allows me to add my own Update and init functions Added IDE folders to ignore list

---
+++ Assets/Scripts/BaseBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Base Behaviour, that all scene components will implement
+//Will allow me to add my own custom update loop since normal Update(); is slow
+public class BaseBehaviour : MonoBehaviour
+{
+   private string _ID;
+   public  string ID => !string.IsNullOrEmpty(_ID) ? _ID : (_ID = GetType().Name);
+
+   public void Log(string Message)   => Debug.Log($"[{ID}] {Message}");
+   public void Warn(string Message)  => Debug.LogWarning($"[{ID}] {Message}");
+   public void Error(string Message) => Debug.LogError($"[{ID}] {Message}");
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : BaseBehaviour
+{
+   public static GameManager Instance { get; private set; }
+   
+   [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+   static void Init()
+   {
+      GameObject GameManager_Prefab = Resources.Load<GameObject>("PF_GameManager");
+      if (GameManager_Prefab == null)
+      {
+         Debug.LogWarning("Failed to find GameManager prefab, parts of the game wont work");
+         return;
+      }
+
+      GameObject GameManager_Instance = Instantiate(GameManager_Prefab);
+      Instance = GameManager_Instance.GetComponent<GameManager>();
+      if (Instance == null)
+      {
+         Debug.LogWarning("GameManager Prefab doesn't have GameManager component on it!");
+         Destroy(GameManager_Instance);
+         return;
+      }
+      
+      //Place Gamemanager into DontDestoryOnLoad Scene, so it is persistant across all levels
+      DontDestroyOnLoad(GameManager_Instance);
+   }
+}
