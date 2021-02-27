bb441b1360deb1dcc13edba9ce0f2a4ecce08c17
Sean Hall <sean.hall412@gmail.com>
2021-02-27 00:44:42 +0000

    Implemented a Kill Zone, So the Player respawns back at their last safe location

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class KillZone : BaseBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!Player.IsPartOfPlayer(other))
+            return;
+        
+        //Make the player respawn at the last safe location, which will be the last visisted dock
+        Player.Instance.Respawn();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Collider Col = GetComponent<Collider>();
+        if (Col == null)
+            return;
+        
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        Gizmos.color = new Color(1, 0, 0, 0.1f);
+        Gizmos.DrawCube(Col.bounds.center, Col.bounds.extents);
+    }
+}
