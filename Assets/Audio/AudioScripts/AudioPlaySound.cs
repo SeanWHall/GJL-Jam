831a968ff12cea5b41753ae12ea4b47c2cd7d4c8
T-Bennett-Audio <63710504+T-Bennett-Audio@users.noreply.github.com>
2021-02-27 21:16:04 +0000

    Audio - Worked on Ambience switchover in DevTesting scene.

---
+++ Assets/Audio/AudioScripts/AudioPlaySound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlaySound : MonoBehaviour
+{
+    private AudioSource myAudioSource; // a private reference to an AudioSource variable
+    public AudioClip mySound; // a public reference to an AudioClip (sound)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>(); // finds the AudioSource component within the GameObject
+    }
+
+    // OnTriggerEnter is called when an actor enters the collider
+    void PlaySound()
+    {
+        myAudioSource.clip = mySound;
+        myAudioSource.Play();
+    }
+}
---
+++ Assets/Audio/AudioScripts/AudioSnapshotCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSnapshotCollider : MonoBehaviour
+{
+    public AudioMixerSnapshot ColliderSnapshot;
+    public float SnapshotTransitionTime;
+
+    // OnTriggerEnter is called when an actor enters the collider
+    void OnTriggerEnter(Collider other)
+    {
+        ColliderSnapshot.TransitionTo(SnapshotTransitionTime);
+    }
+
+}
