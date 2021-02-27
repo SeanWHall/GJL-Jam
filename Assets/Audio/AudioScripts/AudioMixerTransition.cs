f4ac657ba5b377a3f187a31936bb9f0f1c353742
T-Bennett-Audio <63710504+T-Bennett-Audio@users.noreply.github.com>
2021-02-27 01:27:23 +0000

    Audio - Implemented Tsunami weather sounds, and created mixertransition script.

---
+++ Assets/Audio/AudioScripts/AudioMixerTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerTransition : MonoBehaviour
+{
+    public AudioMixerSnapshot nextSnapshot;
+    public float transitionTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSnapshot.TransitionTo(transitionTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
