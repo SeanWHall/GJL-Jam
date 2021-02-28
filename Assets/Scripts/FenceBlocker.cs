cdcc9eb79af7a051dba2046217f3b9d7a56c4b83
Sean Hall <sean.hall412@gmail.com>
2021-02-28 20:14:52 +0000

    Setup a very simple break script, just checks if you have the cutters and uses them

---
+++ Assets/Scripts/FenceBlocker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FenceBlocker : BaseBehaviour, IInteractable
+{
+   public int        InteractionPriority  => 10;
+   public float      InteractionDistance  => 5f;
+   public string     InteractionText      => "Cut Fence";
+   public Vector3    Position             => transform.position;
+   public Material[] InteractionMaterials { get; private set; }
+
+   public override void OnEnable()
+   {
+      base.OnEnable();
+      InteractionMaterials = this.CollectAllMaterials();
+   }
+   
+   public bool CanInteract(Player player) => player.GetInventoryItemIDx("Cutters") != -1;
+   public void OnInteract(Player player) => gameObject.SetActive(false);
+}
